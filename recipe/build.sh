c8fdef022694189d0b90427d836d72208706b414
Nathan Wendt <nathan.wendt@noaa.gov>
2026-05-31 21:10:23 -0500

    Try yet again

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,4 +3,11 @@
 # export ECCODES_LIBS="-L$PREFIX/lib"
 # export ECCODES_CPPFLAGS="-I$PREFIX/include"
 
-$R CMD INSTALL --build .
+if [[ "${target_platform}" != "${build_platform}" ]]; then
+    export DISABLE_NATIVE_TESTS=1
+    R_ARGS="--no-test-load"
+else
+    R_ARGS=""
+fi
+
+${R} CMD INSTALL --build . ${R_ARGS}
